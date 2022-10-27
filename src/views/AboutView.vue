ee904cd17a8da9508848ecc8a1a1ae2f0e804fd3
Simon Minton <simon@minton.co>
2022-10-27 17:22:31 +0100

    test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,17 @@
 <script>
 
 import Clock from "../components/Clock.vue";
+import DisplayBoard from "../components/DisplayBoard.vue";
 export default {
   components: {
-
     Clock,
+    DisplayBoard,
   },
 }
 </script>
 <template>
   <main>
-    
+    <DisplayBoard />
     <Clock />
   </main>
 </template>
