c98c28b49c2984491606322044172b242a5b997a
Simon Minton <simon@minton.co>
2022-10-27 18:27:03 +0100

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,8 @@ export default {
   data() {
     return {
       stations: null,
+      search: null,
+      filteredStations: [],
     };
   },
   mounted() {
@@ -26,9 +28,10 @@ export default {
           // if the stoppointNaptan doesn't start with 940GZZLU, remove it
           // this is a hack to remove the non-tube stations
           data = data.stopPoints.filter((station) => {
-            console.log(station);
             return station.naptanId.startsWith("940GZZLU");
           });
+          // sort stations alphabetically
+          
           // Add a property that is the last three digits of the naptanId
           // this is used to create the URL
           data.forEach((station) => {
@@ -38,18 +41,51 @@ export default {
               ""
             );
           });
-
+          data.sort((a, b) => {
+            if (a.commonName < b.commonName) {
+              return -1;
+            }
+            if (a.commonName > b.commonName) {
+              return 1;
+            }
+            return 0;
+          });
           this.stations = data;
-        });
+        })
+      },
+      filterStationsBySearch() {
+        if (this.search) {
+          this.filteredStations = this.stations.filter((station) => {
+            return station.commonName
+              .toLowerCase()
+              .includes(this.search.toLowerCase());
+          });
+        } else {
+          this.filteredStations = null;
+          return this.stations;
+        }
+      },
+      
     },
-  },
-};
+    watch: {
+      search: function (val) {
+        this.filteredStations = this.stations.filter((station) => {
+          return station.commonName.toLowerCase().includes(val.toLowerCase());
+        });
+      },
+    },
+  }
 </script>
 
 <template>
   <main>
-    <div class="flex w-full px-2">
-      <ul class="flex w-full flex-row flex-wrap">
+    <Clock />
+    <div class="flex w-full px-2 flex-wrap">
+      <div class="w-full justify-center">
+      <input class=" max-w-sm w-small bg-gray-800 p-2 mb-4 w-96 mx-auto flex text-white rounded-md border-none" type="text" placeholder="Search for Station" v-model="search" />
+
+      </div>
+      <ul v-if="!filteredStations.length" class="flex w-full flex-row flex-wrap">
         <li
           class="w-full sm:w-1/2 md:w-1/3 2xl:w-1/4"
           v-for="(station, indx) in stations"
@@ -57,6 +93,23 @@ export default {
         >
           <a :href="'/' + station.shortId">
             <div class="py-1">
+              
+              <div class="text-lg text-white font-bold">
+                {{ station.goodName }}
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
+      <ul v-if="filteredStations.length" class="flex w-full flex-row flex-wrap">
+        <li
+          class="w-full sm:w-1/2 md:w-1/3 2xl:w-1/4"
+          v-for="(station, indx) in filteredStations"
+          v-bind:key="indx"
+        >
+          <a :href="'/' + station.shortId">
+            <div class="py-1">
+              
               <div class="text-lg text-white font-bold">
                 {{ station.goodName }}
               </div>
@@ -65,6 +118,6 @@ export default {
         </li>
       </ul>
     </div>
-    <Clock />
+    
   </main>
 </template>
